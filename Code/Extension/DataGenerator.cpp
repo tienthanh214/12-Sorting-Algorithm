44d06aaac7c12ead027f87706d8d4dfc37294cad
tienthanh214 <39771202+tienthanh214@users.noreply.github.com>
2021-01-03 16:27:38 +0700

    Add files via upload

---
+++ Code/Extension/DataGenerator.cpp
@@ -0,0 +1,74 @@
+﻿#include <time.h>
+#include <cmath>
+#include <fstream>
+#include <iostream>
+
+#include "utilities.h"
+
+using namespace std;
+
+template <class T>
+void HoanVi(T &a, T &b) {
+    T x = a;
+    a = b;
+    b = x;
+}
+
+//-------------------------------------------------
+
+
+// Hàm phát sinh mảng dữ liệu ngẫu nhiên
+void GenerateRandomData(int a[], int n) {
+
+    for (int i = 0; i < n; i++) {
+        a[i] = Rand(0, n - 1);
+    }
+}
+
+// Hàm phát sinh mảng dữ liệu có thứ tự tăng dần
+void GenerateSortedData(int a[], int n) {
+    for (int i = 0; i < n; i++) {
+        a[i] = i;
+    }
+}
+
+// Hàm phát sinh mảng dữ liệu có thứ tự ngược (giảm dần)
+void GenerateReverseData(int a[], int n) {
+    for (int i = 0; i < n; i++) {
+        a[i] = n - 1 - i;
+    }
+}
+
+// Hàm phát sinh mảng dữ liệu gần như có thứ tự
+void GenerateNearlySortedData(int a[], int n) {
+    for (int i = 0; i < n; i++) {
+        a[i] = i;
+    }
+    srand((unsigned int)time(NULL));
+    for (int i = 0; i < 10; i++) {
+        int r1 = Rand(0, n - 1);
+        int r2 = Rand(0, n - 1);
+        HoanVi(a[r1], a[r2]);
+    }
+}
+
+void GenerateData(int a[], int n, int dataType) {
+    srand((unsigned int)time(NULL));
+
+    switch (dataType) {
+        case 0:  // ngẫu nhiên
+            GenerateRandomData(a, n);
+            break;
+        case 1:  // có thứ tự
+            GenerateSortedData(a, n);
+            break;
+        case 2:  // có thứ tự ngược
+            GenerateReverseData(a, n);
+            break;
+        case 3:  // gần như có thứ tự
+            GenerateNearlySortedData(a, n);
+            break;
+        default:
+            printf("Error: unknown data type!\n");
+    }
+}
